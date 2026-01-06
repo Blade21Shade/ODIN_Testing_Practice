b74f5fc1a9e268c28c4f7bcc8560551ea3f377b8
Blade21Shade <blade21shade@gmail.com>
2026-01-06 12:57:41 -0600

    Create capitalize() tests for object and array, and update the comments on tests that throw errors to reflect test results
    index.test.js
    - Add tests for capitalize()
     - Object as argument
     - Array as argument
    - Update comments on tests that expect an error to better reflect that the expectation was met

--- index.test.js
+++ index.test.js
@@ -8,6 +8,14 @@ test("Capitalize test on empty string: '' -> ''", () => {
     expect(toTest.capitalize('')).toBe('');
 });
 
-test("Capitalize test on number: 1 -> ? ** Should fail", () => {
+test("Capitalize test on number: 1 -> Error", () => {
     expect(()=>{toTest.capitalize(1)}).toThrow(); // toThrow needs expect() to use a wrapper, otherwise the error is just thrown normally
+});
+
+test("Capitalize test on object: {} -> Error", () => {
+    expect(()=>{toTest.capitalize({})}).toThrow();
+});
+
+test("Capitalize test on array: [] -> Error", () => {
+    expect(()=>{toTest.capitalize([])}).toThrow();
 });
