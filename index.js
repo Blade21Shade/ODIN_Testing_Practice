bc4c72ad2ec35af4240c3ef39585e2a6eb0c3062
Blade21Shade <blade21shade@gmail.com>
2026-01-06 12:52:54 -0600

    Create test for capitalize() to ensure writing a number throws an error and update the function to pass it
    index.js
    - Refactor capitalize(s)
     - Now throws an error if s isn't a string
    
     index.test.js
     - Add test for capitalize to see if it throws an error when a number is given instead of a string

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function capitalize(s) {
+    if (typeof(s) !== 'string') {
+        throw new Error("capitalize() should only take strings as parameters")
+    }
+    
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
--- index.test.js
+++ index.test.js
@@ -7,3 +7,7 @@ test("Capitalize should pass: ace -> Ace", () => {
 test("Capitalize test on empty string: '' -> ''", () => {
     expect(toTest.capitalize('')).toBe('');
 });
+
+test("Capitalize test on number: 1 -> ? ** Should fail", () => {
+    expect(()=>{toTest.capitalize(1)}).toThrow(); // toThrow needs expect() to use a wrapper, otherwise the error is just thrown normally
+});
