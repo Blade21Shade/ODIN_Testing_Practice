4023d8f6a3d63e442e9a6b2ed3dd40ce5d7ca7c4
Blade21Shade <blade21shade@gmail.com>
2026-01-06 12:43:08 -0600

    Add test to capitalize() for an empty string as the argument

--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
 import * as toTest from "./index.js";
 
-test("Capitalize first letter of a string: ace -> Ace", () => {
+test("Capitalize should pass: ace -> Ace", () => {
     expect(toTest.capitalize('ace')).toBe('Ace');
+});
+
+test("Capitalize test on empty string: '' -> ''", () => {
+    expect(toTest.capitalize('')).toBe('');
 });
