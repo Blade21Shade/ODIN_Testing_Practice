cadcc1f7e42969c7be8d73392e8b3ac607427a46
Blade21Shade <blade21shade@gmail.com>
2026-01-07 12:18:18 -0600

    Restructure test output to make it easier to read
    This just uses a lot of describe()s to structure output
    
    index.test.js
    - Surround tests in describe statements to group their output together when Jest prints
    - In each general describe statement, tests expected to throw errors are nested in another describe statement
    - Since describe statements now group tests, each test message is much shorter to just show what it is testing

--- index.test.js
+++ index.test.js
@@ -1,80 +1,93 @@
 import * as toTest from "./index.js";
 
-// capitalize(s)
-test("Capitalize should pass: ace -> Ace", () => {
-    expect(toTest.capitalize('ace')).toBe('Ace');
+describe("capitalize(s) tests", () => {
+    test("Basic: ace -> Ace", () => {
+        expect(toTest.capitalize('ace')).toBe('Ace');
+    });
+
+    test("Empty string: '' -> ''", () => {
+        expect(toTest.capitalize('')).toBe('');
+    });
+
+    describe("Throw error tests", () => {
+        test("Number: 1 -> Error", () => {
+            expect(()=>{toTest.capitalize(1)}).toThrow(); // toThrow needs expect() to use a wrapper, otherwise the error is just thrown normally
+        });
+
+        test("Object: {} -> Error", () => {
+            expect(()=>{toTest.capitalize({})}).toThrow();
+        });
+
+        test("Array: [] -> Error", () => {
+            expect(()=>{toTest.capitalize([])}).toThrow();
+        });
+    });
 });
 
-test("Capitalize test on empty string: '' -> ''", () => {
-    expect(toTest.capitalize('')).toBe('');
+describe("reverseString(s) tests", () => {
+    test("Basic: ace -> eca", () => {
+        expect(toTest.reverseString('ace')).toBe('eca');
+    });
+
+    test("Empty string: '' -> ''", () => {
+        expect(toTest.reverseString('')).toBe('');
+    });
+
+    describe("Throw error tests", () => {
+        test("Number: 1 -> Error", () => {
+            expect(()=>{toTest.reverseString(1)}).toThrow(); // toThrow needs expect() to use a wrapper, otherwise the error is just thrown normally
+        });
+
+        test("Object: {} -> Error", () => {
+            expect(()=>{toTest.reverseString({})}).toThrow();
+        });
+
+        test("Array: [] -> Error", () => {
+            expect(()=>{toTest.reverseString([])}).toThrow();
+        });
+    });
 });
 
-test("Capitalize test on number: 1 -> Error", () => {
-    expect(()=>{toTest.capitalize(1)}).toThrow(); // toThrow needs expect() to use a wrapper, otherwise the error is just thrown normally
-});
+describe("Calculator tests", () => {
+    test("Add, basic: 1, 2 -> 3", () => {
+        expect(toTest.calculator.add(1, 2)).toBe(3);
+    });
 
-test("Capitalize test on object: {} -> Error", () => {
-    expect(()=>{toTest.capitalize({})}).toThrow();
-});
+    test("Subtract, basic: 1, 2 -> -1", () => {
+        expect(toTest.calculator.subtract(1, 2)).toBe(-1);
+    });
 
-test("Capitalize test on array: [] -> Error", () => {
-    expect(()=>{toTest.capitalize([])}).toThrow();
-});
+    test("Multiply, basic: 2, 3 -> 6", () => {
+        expect(toTest.calculator.multiply(2, 3)).toBe(6);
+    });
 
-// reverseString(s)
-test("Reverse should pass: ace -> eca", () => {
-    expect(toTest.reverseString('ace')).toBe('eca');
-});
+    test("Divide, basic: 6, 2 -> 3", () => {
+        expect(toTest.calculator.divide(6, 2)).toBe(3);
+    });
 
-test("Reverse test on empty string: '' -> ''", () => {
-    expect(toTest.reverseString('')).toBe('');
-});
+    describe("Throw error tests", () => {
+        beforeAll(() => {
+            console.log("A helper function is used to validate input in each function, so each type of invalid input only needs tested once")
+        });
 
-test("Reverse test on number: 1 -> Error", () => {
-    expect(()=>{toTest.reverseString(1)}).toThrow(); // toThrow needs expect() to use a wrapper, otherwise the error is just thrown normally
-});
+        test("No parameters: -> Error", () => {
+            expect(()=>{toTest.calculator.add()}).toThrow();
+        });
 
-test("Reverse test on object: {} -> Error", () => {
-    expect(()=>{toTest.reverseString({})}).toThrow();
-});
+        test("String char: 'a', 'b' -> Error", () => {
+            expect(()=>{toTest.calculator.add('a', 'b')}).toThrow();
+        });
 
-test("Reverse test on array: [] -> Error", () => {
-    expect(()=>{toTest.reverseString([])}).toThrow();
-});
+        test("String num: '1', '2' -> Error", () => {
+            expect(() => {toTest.calculator.add('1', '2')}).toThrow();
+        });
 
-// calculator object: add, subtract, multiply, divide
-test("Calculator Add, valid input: 1, 2 -> 3", () => {
-    expect(toTest.calculator.add(1, 2)).toBe(3);
-});
+        test("Array: [], 1 -> Error", () => {
+            expect(()=>{toTest.calculator.add([], 1)}).toThrow();
+        });
 
-test("Calculator Subtract, valid input: 1, 2 -> -1", () => {
-    expect(toTest.calculator.subtract(1, 2)).toBe(-1);
-});
-
-test("Calculator Multiply, valid input: 2, 3 -> 6", () => {
-    expect(toTest.calculator.multiply(2, 3)).toBe(6);
-});
-
-test("Calculator Divide, valid input: 6, 2 -> 3", () => {
-    expect(toTest.calculator.divide(6, 2)).toBe(3);
-});
-
-test("Calculator invalid input: '1', '2' -> Error", () => {
-    expect(() => {toTest.calculator.add('1', '2')}).toThrow();
-});
-
-test("Calculator invalid input: no parameters -> Error", () => {
-    expect(()=>{toTest.calculator.add()}).toThrow();
-});
-
-test("Calculator invalid input: 'a', 'b' -> Error", () => {
-    expect(()=>{toTest.calculator.add('a', 'b')}).toThrow();
-});
-
-test("Calculator invalid input: [], 1 -> Error", () => {
-    expect(()=>{toTest.calculator.add([], 1)}).toThrow();
-});
-
-test("Calculator invalid input: {}, 1 -> Error", () => {
-    expect(()=>{toTest.calculator.add({}, 1)}).toThrow();
+        test("Object: 1, {} -> Error", () => {
+            expect(()=>{toTest.calculator.add(1, {})}).toThrow();
+        });
+    });
 });
